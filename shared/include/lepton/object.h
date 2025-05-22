36da4a0c46c7a17f0c685b769061679ee7196fac
rickyadastra <tanas.riccardo03@gmail.com>
2025-05-22 16:29:08 +0200

    feat(lepton): update OBJECT_METHODS macro to include initialization method

--- shared/include/lepton/object.h
+++ shared/include/lepton/object.h
@@ -2,8 +2,9 @@
 
 #include "class.h"
 
-#define OBJECT_METHODS(M, T) \
-    M(T, Class*, get_class, T* self)
+#define OBJECT_METHODS(S, M, T) \
+    M(T, S, T, init) \
+    M(T, S, Class*, get_class, T* self)
 
 BASECLASSDEF(Object, OBJECT_METHODS)
 
--- shared/src/lepton/object.c
+++ shared/src/lepton/object.c
@@ -1,31 +1,27 @@
+#include "lepton/class.h"
 #include <bool.h>
 #include <lepton/object.h>
 #include <null.h>
 
-DECLAREMETHODS(Object, OBJECT_METHODS)
+DECLAREMETHODS(Object, ROOT, OBJECT_METHODS)
 
 Class* Object_get_class(Object* self) {
     return self->classmeta;
 }
 
-#define PACKAGECLASS(CLASS, SUPER) \
-    CLASSMETADEF(CLASS, SUPER) \
-    INITIALIZERDEF(CLASS)
-
-#define INITIALIZERDEF(CLASS) \
-    Bool _##CLASS##_lazy_initialized = false; \
-    \
-    void _##CLASS##_lazy() { \
-        I##NAME = (I##NAME) { \
-            
-        }; \
-    }
-
-Object _Object_init() {
-    if (!_Object_)
-    return (Object) {
-        .classmeta = &_Object_classmeta
+Object Object_init() {
+    return (Object){
+        .classmeta = &_Object_classmeta,
     };
 }
 
-PACKAGECLASS(Object, ROOT)
+Class _Object_classmeta = {
+    .name = "Object",
+    .super_class = ((Class *)((void *)0)),
+    .interface = &IObject
+};
+
+ObjectInterface IObject = {
+    .get_class = Object_get_class,
+    .init = Object_init,
+};
