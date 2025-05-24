2da81e7b78f6bedfa303571362bd8a10fc372f39
rickyadastra <tanas.riccardo03@gmail.com>
2025-05-24 03:36:39 +0200

    feat(lepton): enhance UNWRAP macro to include exception handling

--- shared/include/lepton/result.h
+++ shared/include/lepton/result.h
@@ -17,4 +17,13 @@ typedef enum {
 #define RESULT(T, DATA) (T##Result) {.status = RESULT_STATUS_PRESENT, .data = DATA} 
 
 #define IS_PRESENT(RESULT) (RESULT.status == RESULT_STATUS_PRESENT)
-#define UNWRAP(RESULT) (typeof(RESULT.data)) RESULT.data
+#define UNWRAP(RESULT) \
+    (RESULT.status == RESULT_STATUS_PRESENT) ?  \
+        (typeof(RESULT.data))RESULT.data :  \
+        ({ \
+            Exception t = IException.init(); \
+            IException.set(&t, "ResultException", __FILE__, __LINE__); \
+            THROW(t); \
+            (typeof(RESULT.data)) 0; \
+        })
+#define UNSAFE_UNWRAP(RESULT) (typeof(RESULT.data)) RESULT.data
