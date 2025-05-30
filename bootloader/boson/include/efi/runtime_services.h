b2702309cf8be24d02d3c3119d07a6c6faa68536
rickyadastra <tanas.riccardo03@gmail.com>
2025-05-30 02:14:35 +0200

    style(boson): format runtime_services.h for improved readability

--- bootloader/boson/include/efi/runtime_services.h
+++ bootloader/boson/include/efi/runtime_services.h
@@ -1,37 +1,37 @@
+#pragma once
+
 #include <int.h>
 #include <efi/efi.h>
 #include <efi/time.h>
 
-#pragma once
-
 #define EFI_RUNTIME_SERVICES_SIGNATURE 0x56524553544e5552
 #define EFI_RUNTIME_SERVICES_REVISION EFI_SPECIFICATION_VERSION
 typedef struct {
-    EfiTableHeader                 Hdr;
+    EfiTableHeader    Hdr;
 
     // Time Services
-    EfiGetTime                       GetTime;
-    void*                     SetTime;
-    void*              GetWakeupTime;
-    void*              SetWakeupTime;
+    EfiGetTime GetTime;
+    void*      SetTime;
+    void*      GetWakeupTime;
+    void*      SetWakeupTime;
 
     // Virtual Memory Services
     void*      SetVirtualAddressMap;
-    void*                ConvertPointer;
+    void*      ConvertPointer;
 
     // Variable Services
-    void*                 GetVariable;
-    void*       GetNextVariableName;
-    void*                 SetVariable;
+    void*      GetVariable;
+    void*      GetNextVariableName;
+    void*      SetVariable;
 
     // Miscellaneous Services
-    void*     GetNextHighMonotonicCount;
-    void*                 ResetSystem;
+    void*      GetNextHighMonotonicCount;
+    void*      ResetSystem;
 
     // UEFI 2.0 Capsule Services
-    void*               UpdateCapsule;
-    void*   QueryCapsuleCapabilities;
+    void*      UpdateCapsule;
+    void*      QueryCapsuleCapabilities;
 
     // Miscellaneous UEFI 2.0 Service
-    void*          QueryVariableInfo;
+    void*      QueryVariableInfo;
 } EfiRuntimeServices;
