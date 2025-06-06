decf0aadc1292ed9bd1c864bc4d643d59ecff09a
rickyadastra <tanas.riccardo03@gmail.com>
2025-06-07 01:38:37 +0200

    refactor: remove unused string manipulation function declarations

--- bootloader/boson/include/string.h
+++ bootloader/boson/include/string.h
@@ -4,7 +4,4 @@
 #include <vargs.h>
 #include <wchar.h>
 
-unsigned int strlen(const char* str);
-char* ltoa(UInt64 i, unsigned base, char* buf);
-void ltoa_s(Int64 i, unsigned base, char* buf);
 WChar16* vstrf(const char* istr, WChar16* buffer, VAList args);
