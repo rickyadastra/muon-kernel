c1869bdd2944b0d30ac68f9b20c6894b86b18c0f
rickyadastra <tanas.riccardo03@gmail.com>
2025-06-10 23:49:18 +0200

    feat: add memory management and utility functions to utils.h

--- bootloader/boson/include/utils/utils.h
+++ bootloader/boson/include/utils/utils.h
@@ -12,6 +12,9 @@ EfiStatus efi_logf(const char* s, ...);
 EfiInputKey efi_pause();
 EfiStatus efi_alloc(Size size, void** buffer);
 EfiStatus efi_free(void* buffer);
+EfiStatus efi_alloc_pages(Size size, UPtr* addr);
+void efi_mem_copy(const char* from, char* to, Size size);
+void efi_mem_set(char* to, Size size, char value);
 
 extern EfiSimpleTextOutputProtocol* efiOut;
 extern EfiSimpleTextInputProtocol* efiIn;
