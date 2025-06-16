9cf275f97c2dce7bc72ac7138ed65b74e8c7c2ca
rickyadastra <tanas.riccardo03@gmail.com>
2025-06-17 01:57:56 +0200

    feat: add MemoryManagerException class for memory management error handling

---
+++ bootloader/boson/include/exception/memory_exception.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <lepton/core.h>
+#include <exception/bootloader_exception.h>
+
+#define _MEMORYMANAGER_EXCEPTION_METHODS(TYPE, SUPER, FUNC)
+
+#define MEMORYMANAGER_EXCEPTION_METHODS(TYPE, SUPER, FUNC) \
+    THROWABLE_METHODS(TYPE, SUPER, FUNC) \
+    _MEMORYMANAGER_EXCEPTION_METHODS(TYPE, SUPER, FUNC)
+
+CLASSDEF(MemoryManagerException, BootloaderException, MEMORYMANAGER_EXCEPTION_METHODS)
+
+ENDCLASSDEF(MemoryManagerException)
