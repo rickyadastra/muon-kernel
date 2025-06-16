bd74d3712ffc2ab26d43357637250b5859e571dc
rickyadastra <tanas.riccardo03@gmail.com>
2025-06-17 01:14:17 +0200

    feat(lepton): add NullException class with methods for exception handling

---
+++ shared/src/lepton/exception/null_exception.c
@@ -0,0 +1,6 @@
+#include <lepton/core/class.h>
+#include <lepton/exception/null_exception.h>
+
+INHERITCLASS(NullException, _NULL_EXCEPTION_METHODS, EXCEPTION_METHODS)
+
+PACKAGECLASS(NullException, Exception, _NULL_EXCEPTION_METHODS, EXCEPTION_METHODS)
