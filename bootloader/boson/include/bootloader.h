ce30261ba4fc645be23a5955bce6296065d35c70
rickyadastra <tanas.riccardo03@gmail.com>
2025-06-17 01:13:10 +0200

    feat: extend Bootloader class with methods for volume management and memory allocation

--- bootloader/boson/include/bootloader.h
+++ bootloader/boson/include/bootloader.h
@@ -1,14 +1,19 @@
 #pragma once
 
 #include "efi/boot_services.h"
+#include <file.h>
 #include <efi/efi.h>
 #include <efi/file_protocol.h>
 #include <efi/image_protocol.h>
 #include <lepton/core.h>
+#include <bool.h>
 
 #define _BOOTLOADER_METHODS(T, S, M) \
     METHOD(T, S, M, void, set_handle, EfiHandle efiHandle) \
     METHOD(T, S, M, void, set_efi_table, EfiSystemTable* efiTable) \
+    METHOD(T, S, M, Bool, open_volume) \
+    METHOD(T, S, M, UPtr, alloc, Size size, UPtr* buffer) \
+    METHOD(T, S, M, void, free, UPtr buffer) \
     
 #define BOOTLOADER_METHODS(T, S, M) \
     OBJECT_METHODS(T, S, M) \
@@ -18,5 +23,7 @@ CLASSDEF(Bootloader, Object, BOOTLOADER_METHODS)
     EfiHandle bootloaderHandle;
     EfiBootServices* bootServices;
     EfiLoadedImageProtocol* bootloaderImage;
+
     EfiSimpleFileSystemProtocol* fileSystem;
+    File root;
 ENDCLASSDEF(Bootloader)
--- bootloader/boson/src/bootloader.c
+++ bootloader/boson/src/bootloader.c
@@ -1,8 +1,13 @@
 #include "bootloader.h"
-#include "efi/efi.h"
-#include "exception/bootloader_exception.h"
-#include "utils/utils.h"
+#include "efi/file_protocol.h"
+#include "file.h"
+#include "int.h"
+#include <lepton/exception/null_exception.h>
+#include <efi/efi.h>
+#include <exception/bootloader_exception.h>
+#include <utils/utils.h>
 #include <lepton/core.h>
+#include <null.h>
 
 INHERITCLASS(Bootloader, _BOOTLOADER_METHODS, OBJECT_METHODS)
 
@@ -11,6 +16,16 @@ void Bootloader_set_handle(Bootloader *self, EfiHandle efiHandle) {
 }
 
 void Bootloader_set_efi_table(Bootloader *self, EfiSystemTable *efiTable) {
+    if (efiTable == null) {
+        THROW_EXCEPTION(NullException, "EfiSystemTable is null")
+        return;
+    }
+
+    if (efiTable->BootServices == null) {
+        THROW_EXCEPTION(NullException, "BootServices is null")
+        return;
+    }
+
     self->bootServices = efiTable->BootServices;
 
     EfiStatus s;
@@ -19,6 +34,31 @@ void Bootloader_set_efi_table(Bootloader *self, EfiSystemTable *efiTable) {
 
     s = self->bootServices->HandleProtocol(self->bootloaderImage->DeviceHandle, &(EfiGUID)EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID, (void**)&self->fileSystem);
     EFI_THROW(s, BootloaderException, "Cannot load file system information")
+}
+
+Bool Bootloader_open_volume(Bootloader *self) {
+    EfiStatus s;
+    
+    EfiFileProtocol* fileRoot;
+    s = self->fileSystem->OpenVolume(self->fileSystem, &fileRoot);
+    EFI_THROW(s, BootloaderException, "Cannot open volume", false)
+
+    self->root = IFile.init();
+    IFile.from(&self->root, fileRoot);
+
+    return true;
+}
+
+UPtr Bootloader_alloc(Bootloader *self, Size size, UPtr* buffer) {
+    EfiStatus s = self->bootServices->AllocatePool(EFI_BOOT_SERVICES_DATA, size, (void**)buffer);
+    EFI_THROW(s, BootloaderException, "Cannot allocate memory pool", (UPtr)0);
+
+    return *buffer;
+}
+
+void Bootloader_free(Bootloader *self, UPtr buffer) {
+    EfiStatus s = self->bootServices->FreePool((void*)buffer);
+    EFI_THROW(s, BootloaderException, "Cannot free memory pool");
 }
 
 PACKAGECLASS(Bootloader, Object, _BOOTLOADER_METHODS, OBJECT_METHODS)
