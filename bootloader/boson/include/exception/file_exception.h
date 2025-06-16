df27ca8b5ff43d74d9d9d7ee5a67926a844153b5
rickyadastra <tanas.riccardo03@gmail.com>
2025-06-17 01:12:46 +0200

    feat: add FileException class with methods for exception handling

---
+++ bootloader/boson/include/exception/file_exception.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <lepton/core.h>
+#include <exception/bootloader_exception.h>
+
+#define _FILE_EXCEPTION_METHODS(TYPE, SUPER, FUNC)
+
+#define FILE_EXCEPTION_METHODS(TYPE, SUPER, FUNC) \
+    THROWABLE_METHODS(TYPE, SUPER, FUNC) \
+    _FILE_EXCEPTION_METHODS(TYPE, SUPER, FUNC)
+
+CLASSDEF(FileException, BootloaderException, FILE_EXCEPTION_METHODS)
+
+ENDCLASSDEF(FileException)
