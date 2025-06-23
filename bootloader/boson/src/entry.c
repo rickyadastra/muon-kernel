2721dbe8ae1a4c48f93ea4adfe1a17de46398020
rickyadastra <tanas.riccardo03@gmail.com>
2025-06-24 01:30:39 +0200

    refactor: reorganize includes and remove unused virtual_map function

--- bootloader/boson/src/entry.c
+++ bootloader/boson/src/entry.c
@@ -1,27 +1,19 @@
 #include "bootloader.h"
 #include "console.h"
 #include "efi/memory.h"
-#include <elf/elf_parser.h>
-#include "lepton/exception/exception.h"
 #include "memory_manager.h"
-#include <lepton/core.h>
+#include "lepton/exception/exception.h"
+#include <efi/efi.h>
 #include <efi/status.h>
+#include <elf/elf_parser.h>
+#include <elf/elf.h>
 #include <elf/program.h>
-#include <size.h>
-#include <lepton/result.h>
 #include <lepton/core.h>
+#include <lepton/result.h>
+#include <size.h>
 #include <int.h>
 #include <bool.h>
 #include <null.h>
-#include <efi/efi.h>
-#include <efi/file_protocol.h>
-#include <efi/boot_services.h>
-#include <efi/image_protocol.h>
-#include <efi/time.h>
-#include <efi/runtime_services.h>
-#include <elf/elf.h>
-#include <core/bytearray.h>
-#include <utils/utils.h>
 
 #define APIC_BASE 0xFEE00000
 #define KERNEL_STACK_PAGES 4
@@ -30,14 +22,7 @@
 extern __attribute__((noreturn)) void start_kernel(UPtr stack, UPtr pml4, UPtr entry);
 extern UInt8 trampolineStart, trampolineEnd;
 
-void virtual_map(UPtr paddr, UPtr vaddr, UInt64* pml4);
-
 EfiStatus efi_main(EfiHandle handle, EfiSystemTable* systemTable) {
-    efiOut = systemTable->ConOut;
-    efiIn = systemTable->ConIn;
-    runtimeServices = systemTable->RuntimeServices;
-    bootServices = systemTable->BootServices;
-
     Console console = IConsole.init();
     MemoryManager memManager = IMemoryManager.init();
     Bootloader bootloader = IBootloader.init();
@@ -93,7 +78,6 @@ EfiStatus efi_main(EfiHandle handle, EfiSystemTable* systemTable) {
         
         start_kernel(bootloader.kernelStack.end, (UPtr)bootloader.kernelPageTable, IElfParser.get_entry_point(kernelBuf));
 
-
     } CATCH(Exception, e) {
         IConsole.logf(&console, "%s (%s:%u): %s", IException.get_class(e)->name, e->file, e->line, e->message);
         IConsole.output(&console, L"\nPress any key to return to UEFI shell", false);
@@ -103,43 +87,4 @@ EfiStatus efi_main(EfiHandle handle, EfiSystemTable* systemTable) {
     } ENDTRY
 
     return EFIERR(EFI_LOAD_ERROR);
-}
-
-void virtual_map(UPtr paddr, UPtr vaddr, UInt64* pml4) {
-    paddr = (UInt64)paddr & ~((UInt64)0xfff);
-    vaddr = (UInt64)vaddr & ~((UInt64)0xfff);
-
-    UPtr pml4e = (vaddr >> 39) & 0x1ff;
-    UPtr pdpte = (vaddr >> 30) & 0x1ff;
-    UPtr pde   = (vaddr >> 21) & 0x1ff;
-    UPtr pte   = (vaddr >> 12) & 0x1ff;
-    efi_logf("pml4e: %u > pdpte: %u > pde: %u > pte: %u: %x ==> %x", pml4e, pdpte, pde, pte, vaddr, paddr);
-
-    efi_pause();
-
-    UInt64 *thisPml4 = pml4, *thisPdpt = 0, *thisPd = 0, *thisPt = 0;
-    if (!(thisPml4[pml4e] & EFI_PAGE_PRESENT)) {
-        efi_alloc_pages(EFI_PAGE_SIZE, (UPtr*)&thisPdpt);
-        thisPml4[pml4e] = (UInt64)thisPdpt | EFI_PAGE_PRESENT | EFI_PAGE_RW;
-    } else
-    thisPdpt = (UInt64*)((UInt64)thisPml4[pml4e] & ~((UInt64)0xfff));
-    efi_logf("pdpt %x", thisPdpt);
-
-    if (!(thisPdpt[pdpte] & EFI_PAGE_PRESENT)) {
-        efi_alloc_pages(EFI_PAGE_SIZE, (UPtr*)&thisPd);
-        thisPdpt[pdpte] = (UInt64)thisPd | EFI_PAGE_PRESENT | EFI_PAGE_RW;
-    } else
-    thisPd = (UInt64*)((UInt64)thisPdpt[pdpte] & ~((UInt64)0xfff));
-    efi_logf("pd %x", thisPd);
-
-    if (!(thisPd[pde] & EFI_PAGE_PRESENT)) {
-        efi_alloc_pages(EFI_PAGE_SIZE, (UPtr*)&thisPt);
-        thisPd[pde] = (UInt64)thisPt | EFI_PAGE_PRESENT | EFI_PAGE_RW;
-    } else
-    thisPt = (UInt64*)((UInt64)thisPd[pde] & ~((UInt64)0xfff));
-    efi_logf("pt %x", thisPt);
-
-    if (!(thisPt[pte] & EFI_PAGE_PRESENT)) {
-        thisPt[pte] = paddr | EFI_PAGE_PRESENT | EFI_PAGE_RW;
-    }
 }
