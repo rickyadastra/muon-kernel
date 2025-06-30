747aba74e45abbe59c57cee9d4ab80988306af41
rickyadastra <tanas.riccardo03@gmail.com>
2025-07-01 00:56:34 +0200

    feat: add shared definitions for memory regions and bootloader payload

---
+++ shared/include/boson/boson.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "int.h"
+#include "size.h"
+
+typedef enum {
+    MEMORY_REGION_BOOTLOADER,
+    MEMORY_REGION_AVAILABLE,
+    MEMORY_REGION_FIRMWARE,
+    MEMORY_REGION_ACPI,
+    MEMORY_REGION_MMIO,
+    MEMORY_REGION_RESERVED
+} MemoryRegionType;
+
+typedef struct _Bootloader_Kernel_Stack_struct {
+    Size size;
+    UPtr base;
+    UPtr end;
+} BootloaderKernelStack;
+
+typedef struct _Memory_Region_struct {
+    MemoryRegionType type;
+    UPtr base;
+    BigSize size;
+} MemoryRegion;
+
+typedef struct _Bootloader_Payload_struct {
+    UPtr pageTablePaddr;
+    BootloaderKernelStack stack;
+    MemoryRegion* memoryRegionMap;
+    Size memoryRegionEntries;
+} BootloaderPayload;
+
