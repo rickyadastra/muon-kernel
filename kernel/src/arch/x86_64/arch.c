6ab4daed0dc5787424fcdcf3b35ae67edc02646d
rickyadastra <tanas.riccardo03@gmail.com>
2025-07-22 19:34:23 +0200

    feat: refactor kstart function to use Serial interface for output

--- kernel/src/arch/x86_64/arch.c
+++ kernel/src/arch/x86_64/arch.c
@@ -1,19 +1,12 @@
-#include "devices/port.h"
-#include <base/int.h>
+#include "devices/serial.h"
 
 void kstart() {
     asm("cli");
     
+    Serial logSerial = ISerial.init();
+    ISerial.setup(&logSerial, COM1);
 
-    
-    // IPort.out8(&com1, 4, 0x1e);
-    // IPort.out8(&com1, 0, 0xae);
-
-    char msg[] = "hello world\n";
-
-    // for (Size i=0; i<sizeof(msg); i++) {
-    //     IPort.write(&com1, msg[i]);
-    // }
+    ISerial.write(&logSerial, "hello world\n");
 
     for (;;) {}
 }
